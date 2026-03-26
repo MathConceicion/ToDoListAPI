54b5c24cc14c221a68ec978f8f0e265cc0bfae79
git config --global user.name <matheus.conceicion@aluno.senai.br>
2026-03-26 13:48:24 -0300

    feat: Estrutura para as tabelas e migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models.Entities;
+
+namespace ToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Passo 1 foi criar as entidades (Models/Entities/Usuario.cs e Models/Entities/Tarefa.cs)
+        // Passo 2 é criar o DbContext (Este arquivo) e registrar as entidades como 
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tarefa>()
+                .HasOne(t => t.Usuario)
+                .WithMany(u => u.Tarefas)
+                .HasForeignKey(t => t.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
